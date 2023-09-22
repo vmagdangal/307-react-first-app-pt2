ffd9f8206c98934fa566fa883e2c07eb2fdc8d52
vmagdangal <vmagdangal257@gmail.com>
2023-09-22 14:11:05 -0700

    Begin form implementation

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,26 +1,10 @@
 // src/MyApp.js
 import React, {useState} from 'react';
 import Table from "./Table";
+import Form from './Form';
 
 function MyApp() {
-	const [characters, setCharacters] = useState([
-        {
-            name: "Charlie",
-            job: "Janitor",
-          },
-          {
-            name: "Mac",
-            job: "Bouncer",
-          },
-          {
-            name: "Dee",
-            job: "Aspring actress",
-          },
-          {
-            name: "Dennis",
-            job: "Bartender",
-          },
-	]);  
+  const [characters, setCharacters] = useState([]);
 
 	function removeOneCharacter (index) {
 	    const updated = characters.filter((character, i) => {
@@ -28,13 +12,14 @@ function MyApp() {
 	    });
 	  setCharacters(updated);
 	};
-
-    return (
-        <div className="container">
-            <Table characterData={characters} 
-                removeCharacter={removeOneCharacter} />
-        </div>  
-    );
+  
+  return (
+    <div className="container">
+      <Table characterData={characters} 
+        removeCharacter={removeOneCharacter} />
+      <Form />
+    </div>
+  )
 }
 
 export default MyApp;
